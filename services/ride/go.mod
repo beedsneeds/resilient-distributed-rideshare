115032ec6d1e99e3f3d9ce14109155c95bc179da
Bharadhwaj Ram <bharadhwajkram@gmail.com>
2026-02-15 21:01:33 -0500

    Set up workspaces and proto

---
+++ services/ride/go.mod
@@ -0,0 +1,3 @@
+module example.com/ride
+
+go 1.25.6
--- services/ride/ride.go
+++ services/ride/ride.go
@@ -1,1 +1,11 @@
 package ride
+
+import "fmt"
+
+func ride() {
+	fmt.Println("Hello")
+}
+
+func SayHello() {
+	fmt.Println("hello from ride")
+}
