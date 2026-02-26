0d4d84a03577fb0cf83210a200a1298161d6d479
Bharadhwaj Ram <bharadhwajkram@gmail.com>
2026-02-26 11:22:59 -0500

    Major: sqlc generated and db seeded

--- services/ride/ride.go
+++ services/ride/ride.go
@@ -4,15 +4,11 @@ import (
 	"context"
 	"fmt"
 	"os"
-
-	"github.com/jackc/pgx/v5"
 )
 
 func ride() {
 	fmt.Println("Hello")
-	DATABASE_URL = "postgres://postgres:postgres@localhost:5432/database_name"
-	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
-	// conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+
 }
 
 func SayHello() {
