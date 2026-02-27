e8894e3458754a798eb82f106914d7e4ce8db88f
Bharadhwaj Ram <bharadhwajkram@gmail.com>
2026-02-27 14:30:48 -0500

    Minor: implemented first client request

--- services/rider/main.go
+++ services/rider/main.go
@@ -1,38 +1,74 @@
 package main
 
-// import (
-// 	"context"
-// 	"fmt"
-// 	"log"
-// 	"os"
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"time"
 
-// 	ridepb "github.com/beedsneeds/resilient-distributed-rideshare/proto/ride"
-// 	"google.golang.org/grpc"
-// 	"google.golang.org/grpc/credentials"
-// 	"google.golang.org/grpc/credentials/insecure"
-// )
+	ridepb "github.com/beedsneeds/resilient-distributed-rideshare/proto/ride"
+	riderdata "github.com/beedsneeds/resilient-distributed-rideshare/services/rider/data"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	serverAddr         = flag.String("addr", "localhost:50051", "The server address in the format of host:port")
+	serverHostOverride = flag.String("server_host_override", "x.test.example.com", "The server name used to verify the hostname returned by the TLS handshake")
+)
+
+func requestRide(client ridepb.RideServiceClient, riderId string) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	ride, err := client.RequestRide(ctx, &ridepb.RequestRideRequest{
+		IdempotencyKey: &riderId,
+		RiderId:        &riderId,
+	})
+	if err != nil {
+		log.Fatalf("client.RequestRide failed: %v", err)
+	}
+	log.Printf("Request Complete")
+	r := ride.Ride
+	log.Printf("Ride ID: %s", *r.Id)
+
+}
 
 func main() {
-// 	var opts []grpc.DialOption
-// 	if *tls {
-// 		// if *caFile == "" {
-// 		// 	*caFile = data.Path("x509/ca_cert.pem")
-// 		// }
-// 		// creds, err := credentials.NewClientTLSFromFile(*caFile, *serverHostOverride)
-// 		// if err != nil {
-// 		// 	log.Fatalf("Failed to create TLS credentials: %v", err)
-// 		// }
-// 		// opts = append(opts, grpc.WithTransportCredentials(creds))
-// 	} else {
-// 		opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
-// 	}
+	flag.Parse()
+	var opts []grpc.DialOption
+	// https://github.com/grpc/grpc-go/blob/master/examples/route_guide/client/client.go
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
 
-// 	conn, err := grpc.NewClient(*serverAddr, opts...)
-// 	if err != nil {
-// 		log.Fatalf("could not dial: %v", err)
-// 	}
-// 	defer conn.Close()
+	conn, err := grpc.NewClient(*serverAddr, opts...)
+	if err != nil {
+		log.Fatalf("could not dial: %v", err)
+	}
+	defer conn.Close()
+	client := ridepb.NewRideServiceClient(conn)
 
-// 	client := ridepb.NewRideServiceClient(conn)
+	DATABASE_URL := "postgres://postgres:postgres@rider-db:5432/rider_db"
+	dbconn, err := pgx.Connect(context.Background(), DATABASE_URL)
+	// dbconn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("unable to connect to database: %v", err)
+	}
+	defer dbconn.Close(context.Background())
+
+	queries := riderdata.New(dbconn)
+
+	// Get an available rider
+	rider, err := queries.GetRandomAvailableRider(context.Background())
+	if err != nil {
+		log.Fatalf("query failed: %v", err)
+	}
+	fmt.Printf("Rider: %v\n", rider)
+	uuidVal, _ := uuid.FromBytes(rider.ID.Bytes[:])
+	riderId := uuidVal.String()
+
+	requestRide(client, riderId)
 
 }
